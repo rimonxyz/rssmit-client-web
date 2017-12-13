c6d5afc4bd4aaa8d5f974285eda601017dd02d9b
Sayem Hossain <sayemkcn@gmail.com>
2017-12-13 07:09:14 +0600

    changed base url

--- src/app/shared/services/api.endpoints.ts
+++ src/app/shared/services/api.endpoints.ts
@@ -1,5 +1,5 @@
 export const ApiEndpoints = Object.freeze({
-  // BASE_URL: "http://172.104.173.180:9090",
-  BASE_URL: "http://localhost:8080",
+  BASE_URL: "http://172.104.173.180:9090",
+  // BASE_URL: "http://localhost:8080",
   API_VERSION: "/api/v1"
 });
