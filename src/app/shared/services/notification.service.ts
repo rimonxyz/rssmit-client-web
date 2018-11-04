f5387ef4b5d6d01513e02a51a8c1025cd1889f9b
Sayem Hossain <sayemkcn@gmail.com>
2018-11-04 14:57:28 +0600

    Removed error

--- src/app/shared/services/notification.service.ts
+++ src/app/shared/services/notification.service.ts
@@ -1,9 +1,9 @@
 import {Injectable} from '@angular/core';
-import {Http, Response} from "@angular/http";
-import {Auth} from "./auth.service";
-import {Observable} from "rxjs/Observable";
-import {Credentials} from "../model/credentials.model";
-import {ApiEndpoints} from "./api.endpoints";
+import {Http, Response} from '@angular/http';
+import {Auth} from './auth.service';
+import {Observable} from 'rxjs/Observable';
+import {Credentials} from '../model/credentials.model';
+import {ApiEndpoints} from './api.endpoints';
 
 @Injectable()
 export class NotificationService {
@@ -20,11 +20,11 @@ export class NotificationService {
   }
 
   postNotificationUrl(userId: number, notification: any): string {
-    return ApiEndpoints.BASE_URL + ApiEndpoints.API_VERSION + '/firebase/notify/' + userId + '?access_token=' + this.auth.getAccessToken() + '&title=' + notification.title + '&message=' + notification.message + "&type=" + notification.notificationType;
+    return ApiEndpoints.BASE_URL + ApiEndpoints.API_VERSION + '/firebase/notify/' + userId + '?access_token=' + this.auth.getAccessToken() + '&title=' + notification.title + '&message=' + notification.message + '&type=' + notification.notificationType + '&code=' + notification.code;
   }
 
-  postNotificationToTopicUrl( notification: any): string {
-    return ApiEndpoints.BASE_URL + ApiEndpoints.API_VERSION + '/firebase/notify/topic/' + notification.topic + '?access_token=' + this.auth.getAccessToken() + '&title=' + notification.title + '&message=' + notification.message + "&type=" + notification.notificationType;
+  postNotificationToTopicUrl(notification: any): string {
+    return ApiEndpoints.BASE_URL + ApiEndpoints.API_VERSION + '/firebase/notify/topic/' + notification.topic + '?access_token=' + this.auth.getAccessToken() + '&title=' + notification.title + '&message=' + notification.message + '&type=' + notification.notificationType + '&code=' + notification.code;
   }
 
   private handleError(error: Response) {
